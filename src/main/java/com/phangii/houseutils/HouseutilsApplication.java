445899a6b5482e6dc66430f1bcc8f9280bfb0525
kimbro97 <rlagudwog@gmail.com>
2023-07-12 10:01:54 +0900

    first commit

---
+++ src/main/java/com/phangii/houseutils/HouseutilsApplication.java
@@ -0,0 +1,13 @@
+package com.phangii.houseutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseutilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseutilsApplication.class, args);
+    }
+
+}
